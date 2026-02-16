1269ef2af935dfa7bf7648b2496cfe18ca6f153a
Samuel Navarro <samuel3283@gmail.com>
2026-02-16 16:05:21 -0500

    Update outputs.tf

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -6,14 +6,14 @@ output "cluster_name" {
   value = aws_ecs_cluster.this.name
 }
 
-output "service_id" {
-  value = aws_ecs_service.this.id
-}
+#output "service_id" {
+#  value = aws_ecs_service.this.id
+#}
 
-output "service_name" {
-  value = aws_ecs_service.this.name
-}
+#output "service_name" {
+#  value = aws_ecs_service.this.name
+#}
 
-output "task_definition_arn" {
-  value = aws_ecs_task_definition.this.arn
-}
+#output "task_definition_arn" {
+#  value = aws_ecs_task_definition.this.arn
+#}
