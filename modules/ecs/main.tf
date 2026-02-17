f4785774f26c4d350f2e7b22b267dac46b40dbfc
Samuel Navarro <samuel3283@gmail.com>
2026-02-16 19:44:39 -0500

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -82,70 +82,70 @@ resource "aws_iam_role" "ecs_task" {
 }
 
 # ECS Task Definition
-#resource "aws_ecs_task_definition" "this" {
-#  family                   = var.project_name
-#  network_mode             = "awsvpc"
-#  requires_compatibilities = ["FARGATE"]
-#  cpu                      = var.task_cpu
-#  memory                   = var.task_memory
-#  execution_role_arn       = aws_iam_role.ecs_execution.arn
-#  task_role_arn            = aws_iam_role.ecs_task.arn
-#
-#  container_definitions = jsonencode([
-#    {
-#      name      = var.container_name
-#      image     = var.container_image
-#      essential = true
-#      
-#      portMappings = [
-#        {
-#          containerPort = var.container_port
-#          hostPort      = var.container_port
-#          protocol      = "tcp"
-#        }
-#      ]
-#
-#      environment = var.container_environment
-#
-#      logConfiguration = {
-#        logDriver = "awslogs"
-#        options = {
-#          "awslogs-group"         = aws_cloudwatch_log_group.this.name
-#          "awslogs-region"        = var.aws_region
-#          "awslogs-stream-prefix" = "ecs"
-#        }
-#      }
-#    }
-#  ])
-#
-#  tags = merge(var.tags, {
-#    Name = "${var.project_name}-task-definition"
-#  })
-#}
+resource "aws_ecs_task_definition" "this" {
+  family                   = var.project_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
+  task_role_arn            = aws_iam_role.ecs_task.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = var.container_name
+      image     = var.container_image
+      essential = true
+      
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = var.container_environment
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.this.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+    }
+  ])
+
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-task-definition"
+  })
+}
 #
 ## ECS Service
-#resource "aws_ecs_service" "this" {
-#  name            = "${var.project_name}-service"
-#  cluster         = aws_ecs_cluster.this.id
-#  task_definition = aws_ecs_task_definition.this.arn
-#  desired_count   = var.desired_count
-#  launch_type     = "FARGATE"
-#
-#  network_configuration {
-#    subnets          = var.public_subnet_ids
-#    security_groups  = [var.ecs_tasks_security_group_id]
-#    assign_public_ip = true
-#  }
-#
-#  load_balancer {
-#    target_group_arn = var.target_group_arn
-#    container_name   = var.container_name
-#    container_port   = var.container_port
-#  }
-#
-#  depends_on = [var.target_group_arn]
-#
-#  tags = merge(var.tags, {
-#    Name = "${var.project_name}-service"
-#  })
-#}
+resource "aws_ecs_service" "this" {
+  name            = "${var.project_name}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = var.desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.public_subnet_ids
+    security_groups  = [var.ecs_tasks_security_group_id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = var.target_group_arn
+    container_name   = var.container_name
+    container_port   = var.container_port
+  }
+
+  depends_on = [var.target_group_arn]
+
+  tags = merge(var.tags, {
+    Name = "${var.project_name}-service"
+  })
+}
