c86e68e647ea0a19415d08c8b7a7ccb600603892
Samuel Navarro <samuel3283@gmail.com>
2026-02-17 01:44:38 -0500

    Update main.tf

--- modules/security-groups/main.tf
+++ modules/security-groups/main.tf
@@ -22,6 +22,22 @@ resource "aws_security_group" "ecs_tasks" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow traffic on container port"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow traffic on container port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # Allow all outbound traffic
   egress {
     from_port   = 0
